e9207ac19236735f4f0a0655bf60ad29b0e5b8bc
alessandrogiordano971 <alessandro.giordano971@gmail.com>
2023-06-29 16:20:19 +0200

    Update AllPosts.jsx

--- src/screens/AllPosts.jsx
+++ src/screens/AllPosts.jsx
@@ -12,34 +12,34 @@ export default function AllPosts() {
   };
   useEffect(() => {
     getAllPosts();
-  },[]);
+  }, []);
 
   return (
     <div>
       <section className="featured-posts container">
         <h3 className="text-center text-uppercase pt-4">Featured posts</h3>
         <div className="row">
-          {posts.map((post,index)=>{
-            return <div className="col-lg-4 col-md-4 col-sm-12">
-              <div className="card">
-                <img
-                  src="https://animecorner.me/wp-content/uploads/2022/11/bocchi-the-rock.jpg"
-                  className="card-img-top"
-                  alt="casetta"
-                ></img>
-                <div className="card-body">
-                  <h5 className="card-title">{post.title}</h5>
-                  <p className="card-text">
-                  {post.body}
-                  </p>
-                  <div className="d-grid">
-                    <a href="#" className="btn btn-warning">
-                      Go somewhere
-                    </a>
+          {posts.map((post, index) => {
+            return (
+              <div className="col-lg-4 col-md-4 col-sm-12">
+                <div className="card">
+                  <img
+                    src="https://www.rollingstone.it/wp-content/uploads/2018/07/CREDIT-JUNGWOOK-MOK-pf9765_K1.jpg"
+                    className="card-img-top"
+                    alt="casetta"
+                  ></img>
+                  <div className="card-body">
+                    <h5 className="card-title">{post.title}</h5>
+                    <p className="card-text">{post.body}</p>
+                    <div className="d-grid">
+                      <a href="#" className="btn btn-warning">
+                        Go somewhere
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            );
           })}
         </div>
       </section>
