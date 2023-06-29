bde7babb024f6c1764b43a73f7e859bc118bdea0
Sara Spena <saraspena98@gmail.com>
2023-06-29 12:54:44 +0200

    prova sara

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import NavBar from './components/NavBar';
 import About from './screens/About';
 import Contact from './screens/Contact';
+import  {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
+import Home from './screens/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <Router>
     <div>
-      <NavBar/>
-      <About/>
-      <Contact/>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path='/about' element={<About/>} ></Route>
+        <Route path='/contact' element={<Contact/>} ></Route>
+        <Route path='/' element={<Home/>} ></Route>
+      </Routes>
+      <Footer/>
     </div>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">MyPostBrand</a>
+      <NavLink className="navbar-brand" to="/">MyPostBrand</NavLink>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">About Us</a>
+        <li className="nav-item">
+            <NavLink className="nav-link" to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
+            <NavLink className="nav-link" to="/about">About Us</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/contact">Contact</NavLink>
           </li>
         </ul>
         <form className="d-flex">
