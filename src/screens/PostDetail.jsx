bab96bd7289dcb3ff86f9a485e23d17b27d72ccf
Ck-019 <130491046+Ck-019@users.noreply.github.com>
2023-06-30 12:37:17 +0200

    da

--- src/screens/PostDetail.jsx
+++ src/screens/PostDetail.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import { React, useEffect, useState } from "react";
+import { API_BASE_URL } from "../config/constant";
+import { useParams } from "react-router-dom";
 
 export default function PostDetail() {
-  
-    const{postId,userId}=useParams();
-  
-    return (
-    <div>PostDetail post-{postId} , user-{userId} </div>
-  )
+  const [post, setPost] = useState({});
+  const [user, setUser] = useState({});
+  const { postId, userId } = useParams();
+  const getPostAndUser = () => {
+    axios
+      .get(`${API_BASE_URL}/posts/${postId}`)
+      .then((response) => {
+        setPost(response.data);
+        axios.get(`${API_BASE_URL}/users/${userId}`)
+        .then((response) => {
+          setUser(response.data);
+          debugger
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getPostAndUser();
+  }, []);
+  return (
+    <div>
+      PostDetail post-{postId} , user-{userId}{" "}
+    </div>
+  );
 }
