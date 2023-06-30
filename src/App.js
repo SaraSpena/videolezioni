3bc5060b1bb56fa207174cc8cb6d56f9d4daff0e
nancytara <romantickiwi@gmail.com>
2023-06-30 11:46:56 +0200

    dsada

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import './App.css';
 import NavBar from './components/NavBar';
 import About from './screens/About';
 import Contact from './screens/Contact';
-import  {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
 import Home from './screens/Home';
 import Footer from './components/Footer';
 import AllPosts from './screens/AllPosts';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
     <div>
-      <NavBar></NavBar>
+      
+      <NavBar/>
       <Routes>
+<<<<<<< HEAD
         <Route path='/about' element={<About/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/posts/:postId' element={<AllPosts/>}></Route>
         <Route path='/posts' element={<AllPosts/>}></Route>
+=======
+        <Route path = '/' element={<Home/>}></Route>
+        <Route exact path = '/about' element={<About/>}></Route>
+        <Route path = '/posts' element={<AllPosts/>}></Route>
+        <Route path = '/contact' element={<Contact/>}></Route>
+>>>>>>> parent of 504b671 (Merge branch 'SaraBranch' into Nancy)
       </Routes>
       <Footer/>
     </div>
+    
     </Router>
   );
 }
---
+++ src/screens/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container-fluid '>
+      <h3 className='text-center text-uppercase py-4'>About</h3>
+      <p className='shadow-sm py-4 lh-sm text-center'>LOREM LOREM LOREM LOREM LOREM LORE</p>
+      </div>
+  )
+}
