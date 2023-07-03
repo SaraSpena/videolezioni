6a75c3132fac0119705224ace67ce9edd5c3a0a6
Sara Spena <saraspena98@gmail.com>
2023-07-03 15:20:50 +0200

    f

---
+++ src/screens/Signup.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='container'>
+    <h3 className='text-center text-uppercase pt-4'>Create an account</h3>
+    <div className='mx-auto contact-form-container text-muted shadow-sm rounded p-3 lh-2'>
+        <form>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">Fullname</label>
+                <input type="text" className="form-control" id="name" required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="phone" className="form-label">Contact no.</label>
+                <input type="text" className="form-control" id="phone" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="email" required />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className='d-grid'>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+
+        </form>
+    </div>
+</div>
+  )
+}
+
+export default Signup
